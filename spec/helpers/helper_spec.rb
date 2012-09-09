0ac8c37d25ace4cee08bec7f0281358b5cb167f8
Nat Welch <nat@natwelch.com>
2012-09-09 13:57:54 -0700

    Adds test for markdown.

---
+++ spec/helpers/helper_spec.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + "/../spec_helper.rb"
+
+# Take from http://stackoverflow.com/a/10585921/1063
+describe "m" do
+  it "should render basic Markdown" do
+    output = Braindump.helpers.m "# heading"
+    output.should == "<h1>heading</h1>"
+  end
+end
